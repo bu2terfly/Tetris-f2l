1522b6365efe48bd34e1620ea88b0b62e7d442fa
Code-mayur <166780701+Code-mayur@users.noreply.github.com>
2024-10-05 19:08:54 +0530

    Update users_api.py

--- plugins/users_api.py
+++ plugins/users_api.py
@@ -1,39 +1,28 @@
-# © Telegram : @KingVJ01 , GitHub : @VJBots
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import requests
 import json
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import DB_URI, DB_NAME
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 client = AsyncIOMotorClient(DB_URI)
 db = client[DB_NAME]
 col = db["users"]
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Function to get the shortened link using the user's shortener API and base site
 async def get_short_link(user, link):
     api_key = user["shortener_api"]
     base_site = user["base_site"]
-    print(user)
+    
+    # If base_site is None, return None or handle appropriately
+    if not base_site:
+        return None
+    
     response = requests.get(f"https://{base_site}/api?api={api_key}&url={link}")
     data = response.json()
-    if data["status"] == "success" or rget.status_code == 200:
+
+    if data["status"] == "success" or response.status_code == 200:
         return data["shortenedUrl"]
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Function to retrieve the user from the database or create a new one if not found
 async def get_user(user_id):
 
     user_id = int(user_id)
@@ -52,40 +41,29 @@ async def get_user(user_id):
 
     return user
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-async def update_user_info(user_id, value:dict):
+# Function to update user information, including base_site
+async def update_user_info(user_id, value: dict):
     user_id = int(user_id)
     myquery = {"user_id": user_id}
+    
+    # If the user wants to remove base_site, set it to None in the database
+    if "base_site" in value and value["base_site"] == "None":
+        value["base_site"] = None  # Remove the base_site by setting it to None
+    
     newvalues = { "$set": value }
     await col.update_one(myquery, newvalues)
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Function to get the total number of users
 async def total_users_count():
     count = await col.count_documents({})
     return count
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Function to retrieve all users
 async def get_all_users():
     all_users = col.find({})
     return all_users
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Function to delete a user from the database
 async def delete_user(user_id):
     await col.delete_one({'user_id': int(user_id)})
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+    
