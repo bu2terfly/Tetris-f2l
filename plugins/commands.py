afaad4d7b8ee613d5f64e07e006fed90612c8f2f
Code-mayur <166780701+Code-mayur@users.noreply.github.com>
2024-10-11 13:28:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -16,7 +16,7 @@ import re
 import json
 import base64
 from urllib.parse import quote_plus
-from Tetris.utils.file_properties import get_name, get_hash, get_media_file_size
+from TechVJ.utils.file_properties import get_name, get_hash, get_media_file_size
 
 logger = logging.getLogger(__name__)
 
